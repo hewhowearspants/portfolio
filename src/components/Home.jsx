abe917d3a779134d25df55663e854ac44a11576c
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-25 16:09:13 -0400

    create Home component

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Home = (props) => {
+  return (
+    <div className='home'>
+      <h1>Ryan</h1>
+      <h1>Edwards</h1>
+      <div className='hero'><div className='hero-overlay'></div></div>
+      <p className='blurb'>“As a former Army officer, I developed reliability and a level head through adversity. I do not give up easily, and work to solve difficult problems with a dogged determination. I am self-reliant, able to work independently to quickly absorb new concepts to solve those difficult problems, but at the same time, work well with a team due to my military experience. These are the traits that I bring to a programming environment as a web developer.”</p>
+    </div>
+  )
+}
+
+export default Home;
