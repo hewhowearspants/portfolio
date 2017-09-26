c95900b33ab28ce6b8b525b77c74d213d16fbf35
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-26 13:46:16 -0400

    fill in ContactInfo component

---
+++ src/components/ContactInfo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ContactInfo = (props) => {
+  return (
+    <div className='contact-info'>
+      <p>903 372 0114</p>
+      <p>ryan.deland.edwards@gmail.com</p>
+      <p>https://github.com/hewhowearspants</p>
+    </div>
+  )
+}
+
+export default ContactInfo;
