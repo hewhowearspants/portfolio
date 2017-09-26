7cf70974b9f0760b54eccff40f3d30f0307bbbcf
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-26 13:46:00 -0400

    import and render Nav and Project components

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Home from './components/Home';
+import Nav from './components/Nav';
+import Project from './components/Project';
 
 const projectData = {
   blackjack: {
@@ -57,14 +59,33 @@ class App extends Component {
     this.state = {
       projectData: projectData,
       currentPage: 'home',
+      currentProject: null,
     }
+
+    this.setPage = this.setPage.bind(this);
+    this.setProject = this.setProject.bind(this);
+  }
+
+  setPage(page) {
+    this.setState({
+      currentPage: page,
+    })
+  }
+
+  setProject(project) {
+    this.setState({
+      currentProject: projectData[project],
+    })
   }
 
   render() {
     return (
       <div className="App">
-        <Home />
-
+        <main>
+          {this.state.currentPage === 'home' ? <Home /> : ''}
+          {this.state.currentPage === 'project' ? <Project project={this.state.currentProject} /> : ''}
+        </main>
+        <Nav setPage={this.setPage} setProject={this.setProject} projectData={this.state.projectData} />
       </div>
     );
   }
