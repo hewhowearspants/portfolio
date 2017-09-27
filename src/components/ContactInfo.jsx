23b324ff8e753e492bab8c837c5a7e6bac58ad2d
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-09-27 15:28:40 -0400

    add email link icon, remove link on email text

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -4,8 +4,9 @@ const ContactInfo = (props) => {
   return (
     <div className='contact-info'>
       <p>903 372 0114</p>
-      <a href='mailto:ryan.deland.edwards@gmail.com'>ryan.deland.edwards@gmail.com</a>
+      <p>ryan.deland.edwards@gmail.com</p>
       <div className='links'>
+        <a href='mailto:ryan.deland.edwards@gmail.com'><i className="fa fa-envelope" aria-hidden="true"></i></a>
         <a href='https://github.com/hewhowearspants'><i className="fa fa-github-square" aria-hidden="true"></i></a>
         <a href='https://www.linkedin.com/in/ryan-deland-edwards/'><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
       </div>
