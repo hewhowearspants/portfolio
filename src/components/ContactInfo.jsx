9188fdbcfcf9d74e60c07b57a3b1e968e5350bdd
Ryan Edwards <ryan.deland.edwards@gmail.com>
2017-10-02 09:57:53 -0400

    add link to resume in ContactInfo component

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -6,6 +6,7 @@ const ContactInfo = (props) => {
       <p>903 372 0114</p>
       <p>ryan.deland.edwards@gmail.com</p>
       <div className='links'>
+        <a href='./Ryan Edwards Resume - Web Dev 2017.pdf' title='Resume'><i className="fa fa-file-text" aria-hidden="true"></i></a>
         <a href='mailto:ryan.deland.edwards@gmail.com'><i className="fa fa-envelope" aria-hidden="true"></i></a>
         <a href='https://github.com/hewhowearspants'><i className="fa fa-github-square" aria-hidden="true"></i></a>
         <a href='https://www.linkedin.com/in/ryan-deland-edwards/'><i className="fa fa-linkedin-square" aria-hidden="true"></i></a>
