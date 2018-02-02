4373c7820233eea0c10b870519b52886aeaf8dc0
Ryan Edwards <ryan.deland.edwards@gmail.com>
2018-02-02 16:33:16 -0500

    add functionality to detect when project nav has reached end of scroll

--- src/components/ProjectNav.jsx
+++ src/components/ProjectNav.jsx
@@ -1,6 +1,61 @@
 import React, { Component } from 'react';
 
 class ProjectNav extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      reachedTop: true,
+      reachedBottom: false
+    }
+  }
+
+  componentDidMount() {
+    document.getElementsByClassName('project-nav-inner')[0].addEventListener('scroll', () => this.trackScrolling('Y'));
+    document.getElementsByClassName('project-nav')[0].addEventListener('scroll', () => this.trackScrolling('X'));
+  }
+
+  reachedEnd(el, end) {
+    switch(end) {
+      case 'top':
+        return el.getBoundingClientRect()[end] === document.getElementsByClassName('project-nav-inner')[0].getBoundingClientRect()[end];
+      case 'bottom': 
+        return el.getBoundingClientRect()[end] === document.getElementsByClassName('project-nav-inner')[0].getBoundingClientRect()[end];
+      case 'left':
+        return el.getBoundingClientRect()[end] === window.pageXOffset;
+      case 'right':
+        return el.getBoundingClientRect()[end] === window.innerWidth;
+      default:
+        return;
+    }
+  }
+
+  trackScrolling = (direction) => {
+    let firstProject = Object.values(this.props.projectData).pop().name;
+    let lastProject = Object.values(this.props.projectData)[0].name;
+    const topElement = document.getElementById(firstProject);
+    const bottomElement = document.getElementById(lastProject);
+    if ((this.reachedEnd(bottomElement, 'bottom') && direction === 'Y') || (this.reachedEnd(bottomElement, 'right') && direction === 'X')) {
+      this.setState({
+        reachedBottom: true
+      })
+    } else if (this.state.reachedBottom !== false) {
+      this.setState({
+        reachedBottom: false
+      })
+    }
+    
+    if ((this.reachedEnd(topElement, 'top') && direction === 'Y') || (this.reachedEnd(topElement, 'left') && direction === 'X')) {
+      this.setState({
+        reachedTop: true,
+      })
+    } else if (this.state.reachedTop !== false) {
+      this.setState({
+        reachedTop: false
+      })
+    }
+  }
+
   renderProjects() {
     let projectList = [];
     let projectData = this.props.projectData;
