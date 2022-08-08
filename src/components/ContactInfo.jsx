90c9e7b9c0a316a016813865c3b85002dacee342
Ryan Edwards <ryan.deland.edwards@gmail.com>
2022-08-08 18:28:08 -0500

    Update resume

--- src/components/ContactInfo.jsx
+++ src/components/ContactInfo.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import resume from '../Ryan-Edwards-Resume.pdf';
 
 const ContactInfo = (props) => {
   return (
@@ -6,7 +7,7 @@ const ContactInfo = (props) => {
       <p>903 372 0114</p>
       <p>ryan.deland.edwards@gmail.com</p>
       <div className='links'>
-        <a href='http://ryan-deland-edwards.bitballoon.com/Ryan%20Edwards%20Resume%20-%20Web%20Dev%202018.pdf' title='Resume' alt='Resume' target="_blank" rel="noopener noreferrer"><i className="fas fa-file-alt"></i></a>
+        <a href={resume} title='Resume' alt='Resume' target="_blank" rel="noopener noreferrer"><i className="fas fa-file-alt"></i></a>
         <a href='mailto:ryan.deland.edwards@gmail.com' title='Email' alt='Email'><i className="fas fa-envelope"></i></a>
         <a href='https://github.com/hewhowearspants' title='Github' alt='Github' target="_blank" rel="noopener noreferrer"><i className="fab fa-github-square"></i></a>
         <a href='https://www.linkedin.com/in/ryan-deland-edwards/' title='LinkedIn' alt='LinkedIn' target="_blank" rel="noopener noreferrer"><i className="fab fa-linkedin"></i></a>
